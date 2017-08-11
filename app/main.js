7f202a180f8ec66fab7aa3678e9fdd29cb6124bb
deathart <deathart@hotmail.fr>
2017-08-11 17:44:05 +0200

    Add base electron

---
+++ app/main.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({ width: 1800, height: 1200 });
+
+    mainWindow.loadURL(`file://${__dirname}/html/index.html`);
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
