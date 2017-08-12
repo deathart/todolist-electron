9a5ebc9377b2bfcf003b5d19edce8aec283a4da8
deathart <deathart@hotmail.fr>
2017-08-12 22:13:04 +0200

    Add electron window stats (save app position & size)

--- app/main.js
+++ app/main.js
@@ -1,10 +1,27 @@
 const { ipcMain, electron, app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 let mainWindow;
 
 function createWindow() {
 
-    mainWindow = new BrowserWindow({ width: 1800, height: 1200, minWidth: 1180, minHeight: 600, backgroundColor: '#312450', show: false });
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1300,
+        defaultHeight: 800
+    });
+
+    mainWindow = new BrowserWindow({
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+        minWidth: 1300,
+        minHeight: 800,
+        backgroundColor: '#312450',
+        show: false
+    });
+
+    mainWindowState.manage(mainWindow);
 
     mainWindow.loadURL(`file://${__dirname}/html/index.html`);
 
@@ -38,7 +55,7 @@ app.on('activate', () => {
 ipcMain.on('change-page', function(event, arg) {
     let page_name = arg.name;
     mainWindow.webContents.loadURL(`file://${__dirname}/html/` + page_name + `.html`);
-    if (arg.project_id) { //set project if for project html page
+    if (arg.type == "project") {
         let projectID = arg.project_id;
         mainWindow.webContents.on('did-finish-load', () => {
             mainWindow.webContents.send('project_id', projectID);
