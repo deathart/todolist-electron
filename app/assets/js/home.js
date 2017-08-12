3447513f003856652a6ab87542bea42b75c2e0bc
deathart <deathart@hotmail.fr>
2017-08-12 22:22:34 +0200

    Add home section transition

--- app/assets/js/home.js
+++ app/assets/js/home.js
@@ -2,5 +2,22 @@ const $ = require('jquery');
 const ipcRenderer = require('electron').ipcRenderer;
 
 $("#test").click(function(e) {
-    ipcRenderer.send('change-page', { "name": 'project', "project_id": 5 });
+    ipcRenderer.send('change-page', { "name": 'project', "type": 'project', "project_id": 5 });
+});
+
+$("#home_menu > ul > li").click(function() {
+
+
+    let page_name = $(this).data("page");
+    let block = ".block_" + page_name;
+
+    $(this).addClass("active");
+
+    $("#home_menu > ul > li").not($(this)).removeClass("active");
+
+    $("div #block_select").not($(block)).hide();
+    $(block).show();
+
+    return false;
+
 });
