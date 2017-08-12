21391a3faf4ff0803d8b0376c3bfcd43080822ea
deathart <deathart@hotmail.fr>
2017-08-12 22:30:03 +0200

    add electron-settings settings

--- app/assets/js/home.js
+++ app/assets/js/home.js
@@ -1,12 +1,8 @@
 const $ = require('jquery');
 const ipcRenderer = require('electron').ipcRenderer;
-
-$("#test").click(function(e) {
-    ipcRenderer.send('change-page', { "name": 'project', "type": 'project', "project_id": 5 });
-});
+const settings = require('electron-settings');
 
 $("#home_menu > ul > li").click(function() {
-
 
     let page_name = $(this).data("page");
     let block = ".block_" + page_name;
@@ -20,4 +16,8 @@ $("#home_menu > ul > li").click(function() {
 
     return false;
 
+});
+
+$("#test").click(function(e) {
+    ipcRenderer.send('change-page', { "name": 'project', "type": 'project', "project_id": 5 });
 });
