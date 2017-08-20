e49d7bde742c6188a6868df7e64315a22be78558
deathart <deathart@hotmail.fr>
2017-08-20 21:05:28 +0200

    Add bootstrap framework localy

--- app/assets/js/project.js
+++ app/assets/js/project.js
@@ -1,4 +1,3 @@
-const $ = require('jquery');
 const ipcRenderer = require('electron').ipcRenderer;
 const low = require('lowdb');
 const db = low(process.env.USERPROFILE + "/Documents/todolist-electron/project_list.json");
