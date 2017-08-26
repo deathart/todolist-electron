1605f32851d68e7adfab60718a83c8edd8dd5350
deathart <deathart@hotmail.fr>
2017-08-26 20:15:24 +0200

    Add action button in list task && typo

--- app/assets/js/project.js
+++ app/assets/js/project.js
@@ -95,7 +95,7 @@ $('.addTask-modal').click(function(ev) {
         db.get('projects_info').push({ id: futur_id, projectId: project_id, title: task_title, date_create: task_date_create, type: task_type, dest: task_dest, desc: task_desc, date_finish: task_date_finish, progress: 0 }).write();
         db.get('projects').find({ id: project_id }).assign({ date_lastup: task_date_create }).write();
 
-        $(".list_task").append('<tr data-toggle="collapse" data-target=".collapse_info_' + futur_id + '" class="table-light" data-taskId="' + futur_id + '"><th scope="row">' + futur_id + '</th><td>' + task_title + '</td><td>' + task_type + '</td><td><div class="progress"><div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div></td></tr><tr><td colspan="4" class="hiddenRow"><div class="collapse collapse_info_' + futur_id + '"><div class="info_task_col">' + task_desc + '</div></div></td></tr>');
+        $(".list_task").append('<tr data-toggle="collapse" data-target=".collapse_info_' + futur_id + '" class="table-light" data-taskId="' + futur_id + '"><th scope="row">' + futur_id + '</th><td>' + task_title + '</td><td>' + task_type + '</td><td><div class="progress"><div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div></div></td><td style="text-align: center;"><i class="fa fa-pencil-square-o edit-task" aria-hidden="true"></i><i class="fa fa-trash close-task" aria-hidden="true"></i></td></tr><tr><td colspan="4" class="hiddenRow"><div class="collapse collapse_info_' + futur_id + '"><div class="info_task_col">' + task_desc + '</div></div></td></tr>');
 
         $("#inputTitre").val("");
         $("#inputType").val("");
@@ -112,9 +112,17 @@ $('.addTask-modal').click(function(ev) {
 });
 
 $.each(db.get('projects_info').filter({ projectId: project_id }).value(), function(key, value) {
-    $(".list_task").append('<tr data-toggle="collapse" data-target=".collapse_info_' + value.id + '" class="table-light" data-taskId="' + value.id + '"><th scope="row">' + value.id + '</th><td>' + value.title + '</td><td>' + value.type + '</td><td><div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + value.progress + '%;" aria-valuenow="' + value.progress + '" aria-valuemin="0" aria-valuemax="100">' + value.progress + '%</div></div></td></tr><tr><td colspan="4" class="hiddenRow"><div class="collapse collapse_info_' + value.id + '"><div class="info_task_col">' + value.desc + '</div></div></td></tr>');
+    $(".list_task").append('<tr data-toggle="collapse" data-target=".collapse_info_' + value.id + '" class="table-light" data-taskId="' + value.id + '"><th scope="row">' + value.id + '</th><td>' + value.title + '</td><td>' + value.type + '</td><td><div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + value.progress + '%;" aria-valuenow="' + value.progress + '" aria-valuemin="0" aria-valuemax="100">' + value.progress + '%</div></div></td><td style="text-align: center;"><i class="fa fa-pencil-square-o edit-task" aria-hidden="true"></i><i class="fa fa-trash close-task" aria-hidden="true"></i></td></tr><tr><td colspan="5" class="hiddenRow"><div class="collapse collapse_info_' + value.id + '"><div class="info_task_col">' + value.desc + '</div></div></td></tr>');
 });
 
 $('.collapse').on('show.bs.collapse', function() {
     $('.collapse.show').collapse('hide');
+});
+
+$(".edit-task").click(function() {
+
+});
+
+$(".close-task").click(function() {
+
 });
