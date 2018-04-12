45fb21073cd289c4dd06c4b5022a174f5d597f83
deathart <contact@deathart.fr>
2018-04-12 13:58:37 +0200

    Downgrade gulp & update package scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const plugins = require('gulp-load-plugins')();
 const sassFiles = 'app/assets/scss/**/*.scss';
 const cssDest = 'app/assets/css/';
 
-gulp.task('css', function() {
+gulp.task('scss', function() {
     return gulp.src(sassFiles)
         .pipe(plugins.sass())
         .pipe(plugins.csscomb())
@@ -13,7 +13,7 @@ gulp.task('css', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(sassFiles, ['css']);
+    gulp.watch(sassFiles, ['scss']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['scss']);
